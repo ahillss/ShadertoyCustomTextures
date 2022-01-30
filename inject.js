9358e37a1708bdb99f7dd91ddddfc89d64122dc1
andrewhills <andrewhills@users.noreply.github.com>
2022-01-31 01:17:04 +1100

    cleaned up manifest

--- inject.js
+++ inject.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
 script.src = chrome.runtime.getURL('myscript.js');
-(document.body || document.head).appendChild(script);
+(document.body || document.head || document.documentElement).appendChild(script);
